fbc7b5d97030653bde9835bec9d3ca53e7406694
Vitalii <vs@intellisoftware.net>
2015-09-27 13:57:27 +0300

    Third task(arrays) Part2. Initial data

--- ThirdTask/src/Main.java
+++ ThirdTask/src/Main.java
@@ -9,14 +9,63 @@ public class Main {
         int[] leftArray={1,5,4,23,65,32,78};
         System.out.println("Left array is" +Arrays.toString(leftArray));
         int[] rightArray={3,5,24,4,1,2,34,45,32,5};
-        int[] expectedArray={1,5,4,23,65,32,78,5,4,1,32,5};
-
         int[]result=leftUnion(leftArray, rightArray);
 //        for(int element:result){ //simple print
 //            System.out.println("LeftUnion result is: " + element); //как распечатать одной строкой?
 //        }
         System.out.println("LeftUnion printed with toString "+Arrays.toString(result));
+        testPositive();
+        testLeftArrayIsEmpty();
+        testLeftArrayIsNull();
     }
+
+
+    private static  void testPositive(){
+        int[] leftArray = {1, 5, 4, 23, 65, 32, 78};
+        int[] rightArray = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+        int[] expectedArrays = {1, 5, 4, 23, 65, 32, 78, 5, 4, 1, 32, 5};
+        System.out.println("LeftArray: " + Arrays.toString(leftArray));
+        System.out.println("RightArray: " + Arrays.toString(rightArray));
+
+        Main main =new Main();
+        int[] returnValue=main.leftUnion(leftArray,rightArray);
+
+        System.out.println("ReturnValue: " + Arrays.toString(returnValue));
+        System.out.println("ExpectedArrays: " + Arrays.toString(expectedArrays));
+    }
+
+    private static void testLeftArrayIsEmpty() {
+        int[] leftArray = {};
+        int[] rightArray = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+        int[] expectedArrays = {};
+
+        System.out.println("LeftArray: " + Arrays.toString(leftArray));
+        System.out.println("RightArray: " + Arrays.toString(rightArray));
+
+        Main main = new Main();
+        int[] returnValue = main.leftUnion(leftArray, rightArray);
+
+        System.out.println("ReturnValue: " + Arrays.toString(returnValue));
+        System.out.println("ExpectedArrays: " + Arrays.toString(expectedArrays));
+    }
+
+    private static void testLeftArrayIsNull() {
+        int[] leftArray = null;
+        int[] rightArray = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+        int[] expectedArrays = {};
+
+        System.out.println("LeftArray: " + Arrays.toString(leftArray));
+        System.out.println("RightArray: " + Arrays.toString(rightArray));
+
+        Main main = new Main();
+        int[] returnValue = main.leftUnion(leftArray, rightArray);
+
+        System.out.println("ReturnValue: " + Arrays.toString(returnValue));
+        System.out.println("ExpectedArrays: " + Arrays.toString(expectedArrays));
+    }
+
+
+
 
     public static int[] leftUnion(int[] leftArray, int[] rightArray)
     {
