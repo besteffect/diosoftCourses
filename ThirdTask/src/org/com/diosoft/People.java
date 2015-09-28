b83cddac2978f69d6d20d6c39e398c2ea88ae4ce
Vitalii Sviderskyi <vsv@intellisoftware.net>
2015-09-28 19:53:13 +0300

    Third task(arrays) Part6. Created People and PeopleComparator classes

---
+++ ThirdTask/src/org/com/diosoft/People.java
@@ -0,0 +1,70 @@
+package org.com.diosoft;
+
+/**
+ * Created by vsv on 9/28/2015.
+ */
+public class People {
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public People(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        People people = (People) o;
+
+        if (age != people.age) return false;
+        if (!firstName.equals(people.firstName)) return false;
+        return secondName.equals(people.secondName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + secondName.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ ThirdTask/src/org/com/diosoft/PeopleComparator.java
@@ -0,0 +1,9 @@
+package org.com.diosoft;
+
+import java.util.Comparator;
+
+/**
+ * Created by vsv on 9/28/2015.
+ */
+public abstract class PeopleComparator implements Comparator<People> {
+}
