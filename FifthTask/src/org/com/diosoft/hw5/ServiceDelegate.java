b76a3159d471e3bb9e38a616e8555564e765666b
Vitalii <vs@intellisoftware.net>
2015-10-11 02:06:44 +0300

    Task5 Started ServiceDelegate

---
+++ FifthTask/src/org/com/diosoft/hw5/ServiceDelegate.java
@@ -0,0 +1,12 @@
+package org.com.diosoft.hw5;
+
+public class ServiceDelegate {
+    private final PersonHelper resourceForDelegation;
+    public ServiceDelegate(PersonHelper resourceForDelegation){
+        this.resourceForDelegation=resourceForDelegation;
+    }
+    // Создаем делегат метод который добавляет вывод в консоль входных данных и результатов
+//public void printData  
+
+
+}
