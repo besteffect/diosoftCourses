03048ea41dc6ce345afe24d2228deb7fa4f4bc8e
Vitalii <vs@intellisoftware.net>
2015-10-11 22:20:28 +0300

    7Mock task initial commit

---
+++ 7Mock/service/src/com/diosoft/trsine/calendar/datastore/DataStore.java
@@ -0,0 +1,17 @@
+package com.diosoft.trsine.calendar.datastore;
+
+import com.diosoft.trsine.calendar.common.Event;
+
+import java.util.List;
+
+public interface DataStore {
+
+    void add(Event inputValue);
+
+    List<Event> getEventsByTitle(String inputValue);
+//    Map<UUID,Event>
+//    Map<String, List<UUID>>
+//    Map<Date, List<UUID>>
+
+
+}
